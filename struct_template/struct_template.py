49a1b56347de9a11f9d562d89b6010b4204e7ce7
chenx <bufubaoni@163.com>
2016-09-07 16:51:58 +0800

    edit struct_template less line

--- struct_template/struct_template.py
+++ struct_template/struct_template.py
@@ -6,17 +6,9 @@ B = [2, 3, 4, 5, 6, 7, 8, 2, 3]
 A = [1, [2, 3, [2, 3]], [4, 5, 6], 7]
 
 
-_count = [0]
+_count = iter(range(0, len(B)))
 def B2A(template, input):
-    struct = list()
-    for lst in template:
-        if isinstance(lst, list):
-            struct.append(B2A(lst, input))
-        else:
-            struct.append(input[_count[0]])
-            _count[0] += 1
-    return struct
-
+    return [B2A(lst, input) if isinstance(lst, list) else input[next(_count)] for lst in template]
 
 if __name__ == '__main__':
     print("A------> {a}".format(a=A))
