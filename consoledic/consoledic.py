0329d9bcba83b384e3c44cf7220c8c8325b20a12
chenx <bufubaoni@163.com>
2016-09-16 09:03:18 +0800

    fix ioerror

--- consoledic/consoledic.py
+++ consoledic/consoledic.py
@@ -2,19 +2,23 @@
 import urllib
 import json
 import os
-
+import time 
 os.system("chcp 65001")
 request = urllib.urlopen
 
 def getword(word):
     params=urllib.urlencode({"Word":word})
     f=request("http://xtk.azurewebsites.net/BingDictService.aspx?{params}".format(params=params))
-    getjson=json.loads(f.read())
+    try:
+        getjson=json.loads(f.read())
+        f.close()
+    except:
+        return None
     return getjson
 
 def prettyprint(wordjson):
-    if wordjson["word"]:
-        if wordjson["pronunciation"]["AmE"]:
+    if wordjson and wordjson["word"]:
+        if wordjson["pronunciation"] and wordjson["pronunciation"]["AmE"]:
             print("{word}---/{pronunciation}/".format(word=wordjson["word"],pronunciation=wordjson["pronunciation"]["AmE"].encode("utf8")))
         for index, defd in enumerate(wordjson["defs"]):
             print("{index}-pos:{pos} def:{defd}".format(index=index,pos=defd["pos"],defd=defd["def"].encode("utf8")))
