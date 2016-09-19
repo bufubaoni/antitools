1a54421e56814979a8e7c95bc92c81caad9d028a
chenx <bufubaoni@163.com>
2016-09-19 09:17:43 +0800

    just write a script for test the server of company!

---
+++ server_test/server_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import urllib
+import time
+from datetime import datetime
+
+
+def urlopen(url):
+    try:
+        return urllib.urlopen(url=url)
+    except:
+        return None
+
+def server_test(url):
+    print(datetime.now().isoformat())
+    if urlopen(url):
+        print("The server is runing !")
+    else:
+        print("The server is stop")
+
+def run(url):
+    while True:
+        server_test(url)
+        time.sleep(60*5)
+
+if __name__ == "__main__":
+    run("http://192.168.1.110")
