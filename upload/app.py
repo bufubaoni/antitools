d5735780acc44cdaaeab844ab14d5826373bb890
chenx <bufubaoni@163.com>
2016-09-30 15:10:18 +0800

    anti upload but single file

--- upload/app.py
+++ upload/app.py
@@ -3,17 +3,23 @@
 # Created by Alex on 2016/9/30
 from bottle import route, post, request
 import bottle
-import pdb
 
 app = bottle.default_app()
 
 
 @route("/upload")
+@post("/upload")
 def upload():
-    if request.method.lower() == "port":
-        pdb.set_trace()
+    if request.method.lower() == "post":
+        file = request.files.get("file")
+        file.save("img/" + file.filename)
+        return "ok"
     else:
-        return "upload"
+        return ("<form method='post' action='/upload' enctype='multipart/form-data'>"
+                "<input type='file' name='file'/>"
+                "</br>"
+                "<input type='submit'/>"
+                "</form>")
 
 
 if __name__ == '__main__':
