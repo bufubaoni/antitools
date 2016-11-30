de459dc86df31987acac14846399a914ec672bf8
chenx <bufubaoni@163.com>
2016-11-30 10:54:31 +0800

    add read mysql colunms

---
+++ mysql_colunms/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Alex on 2016/11/30
+
+
+if __name__ == '__main__':
+    pass
---
+++ mysql_colunms/mysql_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Alex on 2016/11/30
+from pydal import DAL, Field
+
+sys_tab = DAL("mysql://lms_test:lmsadmin@192.168.1.110/information_schema")
+
+sys_tab.define_table('COLUMNS',
+                     Field("TABLE_SCHEMA",),
+                     Field("TABLE_NAME"),
+                     Field("COLUMN_NAME"),
+                     Field("IS_NULLABLE"),
+                     Field("DATA_TYPE"),
+                     Field("COLUMN_TYPE"),
+                        primarykey=[],
+                     migrate=False)
+if __name__ == "__main__":
+    print sys_tab(sys_tab.COLUMNS.TABLE_SCHEMA == "lms_test").select()
+    print sys_tab._lastsql
