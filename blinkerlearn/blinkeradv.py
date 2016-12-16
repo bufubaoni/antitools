46e5dd15a5ed92173f14e5421c92fdcdf472b087
chenx <bufubaoni@163.com>
2016-12-16 19:44:15 +0800

    add code

--- blinkerlearn/blinkeradv.py
+++ blinkerlearn/blinkeradv.py
@@ -2,3 +2,18 @@
 # -*- coding: utf-8 -*-
 # Created by Alex on 2016/12/16
 
+from blinker import signal
+
+test_message = signal("test")
+
+
+@test_message.connect
+def test_a(sender):
+    print("this is test a by sender: {sender}".format(sender=sender))
+
+
+@test_message.connect
+def test_b(sender):
+    print("this is test b by sender: {sender}".format(sender=sender))
+
+test_message.send("haha")
