d74adadec49a129f1d1fb02445930f1350369e5b
chenx <bufubaoni@163.com>
2016-12-19 21:44:32 +0800

    random

--- movie/doubanMovie.py
+++ movie/doubanMovie.py
@@ -2,8 +2,18 @@
 # -*- coding: utf-8 -*-
 # Created by Alex on 2016/10/26
 import requests
+import time
+import random
 from pyquery import PyQuery as pq
-
+session = requests.Session()
+url = "https://movie.douban.com/top250"
+session.get(url)
+for i in range(0,10):
+    if i == 0:
+        print i
+    else:
+        print i * 25
+    print random.random(10)
 
 if __name__ == '__main__':
     pass
