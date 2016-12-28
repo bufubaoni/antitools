ae15ab1e2c36f2dabecfb52c4ac797b5d7062ab3
chenx <bufubaoni@163.com>
2016-12-28 17:00:33 +0800

    create scheduler logger

--- huaban/sheduler.py
+++ huaban/sheduler.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by Alex on 2016/12/28
+import logging
+import logging.config
 
+logging.config.fileConfig("logging.conf")
+logger = logging.getLogger("save")
 
 if __name__ == '__main__':
     pass
