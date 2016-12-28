ddb08aba797b6073b640914d2653114118670eb2
chenx <bufubaoni@163.com>
2016-12-28 13:51:34 +0800

    spider huaban

---
+++ huaban/huaban_spider.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Alex on 2016/12/28
+# http://huaban.com/boards/15759013/?qq-pf-to=pcqq.group&ix8ifcer&max=745069813&limit=20&wfl=1
+# http://huaban.com/boards/15759013/?qq-pf-to=pcqq.group&ix8ifces&max=184558005&limit=20&wfl=1
+import requests
+
+
+
+if __name__ == '__main__':
+    pass
