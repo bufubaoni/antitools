520b7a00167f02adff726ddc46125ee4d770911c
chenx <bufubaoni@163.com>
2016-12-28 17:24:45 +0800

    celery add

--- huaban/sheduler.py
+++ huaban/sheduler.py
@@ -3,9 +3,10 @@
 # Created by Alex on 2016/12/28
 import logging
 import logging.config
+from celery import Celery
 
 logging.config.fileConfig("logging.conf")
-logger = logging.getLogger("save")
+logger = logging.getLogger("scheduler")
 
 if __name__ == '__main__':
     pass
