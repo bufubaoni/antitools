0c80b01b32dc477448d24d5a739d4224346d97f1
chenx <bufubaoni@163.com>
2017-01-02 10:38:08 +0800

    readwoff

--- woff/readwoff.py
+++ woff/readwoff.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by Alex on 2017/1/1
+import struct
+
+def woff_headers(infile):
+    WOFFHeader = {'signature': struct.unpack(">I", infile.read(4))[0],
+                  'flavor': struct.unpack(">I", infile.read(4))[0],
+                  'length': struct.unpack(">I", infile.read(4))[0],
+                  'numTables': struct.unpack(">H", infile.read(2))[0],
+                  'reserved': struct.unpack(">H", infile.read(2))[0],
+                  'totalSfntSize': struct.unpack(">I", infile.read(4))[0],
+                  'majorVersion': struct.unpack(">H", infile.read(2))[0],
+                  'minorVersion': struct.unpack(">H", infile.read(2))[0],
+                  'metaOffset': struct.unpack(">I", infile.read(4))[0],
+                  'metaLength': struct.unpack(">I", infile.read(4))[0],
+                  'metaOrigLength': struct.unpack(">I", infile.read(4))[0],
+                  'privOffset': struct.unpack(">I", infile.read(4))[0],
+                  'privLength': struct.unpack(">I", infile.read(4))[0]}
+    return WOFFHeader
+
+def readf(path):
+    with open(path, "rb") as f:
+        print woff_headers(f)
+
+
+if __name__ == "__main__":
+    readf("test.woff")
