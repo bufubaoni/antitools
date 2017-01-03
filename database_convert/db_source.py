0d3466a99cb8adc4d0269a366bd2f0d668cbfde0
chenx <bufubaoni@163.com>
2017-01-03 21:43:33 +0800

    mysql

--- database_convert/db_source.py
+++ database_convert/db_source.py
@@ -4,11 +4,12 @@
 # test1 to test2
 # test1 -> (test1,test2)
 # test2 -> (test1,test3,test4)
-from pydal import DAL,Field
+from pydal import DAL, Field
 
-db_s = DAL("mysql://root:1308200@127.0.0.1/test1", lazy_tables=True, db_codec='utf-8',migrate=False)
+db_s = DAL("mysql://root:1308200@localhost/test1", db_codec='utf-8')
 
 db_s.define_table("test1",
                   Field("test1"),
                   Field("test2"),
-                  primarykey=[])
+                  primarykey=[],
+                  migrate=False)
--- database_convert/test_pro.py
+++ database_convert/test_pro.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by Alex on 2017/1/3
+from db_source import db_s
+
+if __name__ == "__main__":
+    db_s.test1.insert(test1="这个", test2="那个")
