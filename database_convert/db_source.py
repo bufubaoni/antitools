6c69404011ee432c8a8306822961092b3abd134a
chenx <bufubaoni@163.com>
2017-01-03 22:03:31 +0800

    test finished

--- database_convert/db_source.py
+++ database_convert/db_source.py
@@ -6,10 +6,19 @@
 # test2 -> (test1,test3,test4)
 from pydal import DAL, Field
 
-db_s = DAL("mysql://root:1308200@localhost/test1", db_codec='utf-8')
+db_s = DAL("mysql://test:1308200@localhost/test1", db_codec='utf-8')
 
 db_s.define_table("test1",
                   Field("test1"),
                   Field("test2"),
+                  primarykey=[],
+                  migrate=False)
+
+db_arm = DAL("mysql://test:1308200@localhost/test2", db_codec='utf-8')
+
+db_arm.define_table("test2",
+                  Field("test1"),
+                  Field("test3"),
+                  Field("test4"),
                   primarykey=[],
                   migrate=False)
--- database_convert/test_pro.py
+++ database_convert/test_pro.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by Alex on 2017/1/3
-from db_source import db_s
+from db_source import db_s,db_arm
 
 if __name__ == "__main__":
-    db_s.test1.insert(test1="这个", test2="那个")
+    # db_s.test1.insert(test1="这个", test2="那个")
+    # db_s.commit()
+
+    for row in db_s(db_s.test1.test1 != "").iterselect():
+        db_arm.test2.insert(test1=row.test1,test3=row.test2,test4=0)
+        db_arm.commit()
