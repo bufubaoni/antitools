932f8fc39538b955b116187af7094250be5debd8
chenx <bufubaoni@163.com>
2017-01-04 09:27:34 +0800

    add a datetime method

---
+++ learnpython/datetimeconvert.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Alex on 2017/1/4
+from  datetime import datetime
+
+dt = datetime.strptime("20161215", "%Y%m%d")
+print dt
--- leipinlogin/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created by Alex on 2017/1/3
-
-
-if __name__ == '__main__':
-    pass
