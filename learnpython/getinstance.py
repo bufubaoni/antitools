a94cec59adcc73f63720f11294bbecd990f4ab2a
chenx <bufubaoni@163.com>
2017-01-06 09:17:03 +0800

    getinstance

---
+++ learnpython/getinstance.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Alex on 2017/1/6
+class A(object):
+    def saysome(self):
+        print "this is A"
+
+
+if __name__ == '__main__':
+    a = getattr()
