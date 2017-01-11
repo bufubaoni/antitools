51fc3ef5ca51d91336299eee8d0056300791fdd0
chenx <bufubaoni@163.com>
2017-01-11 11:55:06 +0800

    liepin add

--- spider_script/liepin.py
+++ spider_script/liepin.py
@@ -24,7 +24,7 @@ headers = {'Host': 'passport.liepin.com',
            'X-Requested-With': 'XMLHttpRequest',
            'Referer': 'https://passport.liepin.com/ajaxproxy.html',
            'Content-Length': '117',
-           'Cookie': '__tlog=1484103846822.92%7C00000000%7C00000000%7C00000000%7C00000000; abtest=0; verifycode=e163c1bdb3f641a78c7d491e0b5790e1; _fecdn_=1; __uuid=1484103849760.22; __session_seq=1; __uv_seq=1; Hm_lvt_a2647413544f5a04f00da7eee0d5e200=1483428418,1484102217; Hm_lpvt_a2647413544f5a04f00da7eee0d5e200=1484103856; _mscid=00000000; _uuid=8B7BB17AFFA8440B23F0FB0CC04CA6BF; 1f0241fc=ea21a4c007f803dccdbb8595d630d088',
+           'Cookie': '',
            'Connection': 'keep-alive'
            }
 
