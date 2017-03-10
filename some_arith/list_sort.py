5528187d24c1078937b449c48a52b98f88261973
alex <bufubaoni@163.com>
2017-03-10 15:01:07 +0800

    by order to list counter and value

--- some_arith/list_sort.py
+++ some_arith/list_sort.py
@@ -8,9 +8,9 @@ def list_sort(list_source=[]):
     
     _resault = []
 
-    for item in range(max(dic_count.values())):
+    for item in range(max(dic_count.values())+1):
         _resault.append(list())
-
+    
     for item in list_source:
         r_index = -(list_source.count(item))
         if item not in _resault[r_index]:
@@ -18,7 +18,17 @@ def list_sort(list_source=[]):
     
     _resault = reduce(lambda x, y: x + sorted(y, reverse = True), _resault)
     return _resault
-    
+
+
+from collections import Counter
+
+def list_sort_b(list_source=[]):
+    dic_count = Counter(list_source)
+    _r_order_list = sorted(dic_count.items(), lambda x, y: cmp(x[1], y[1]), key=lambda x: x)
+    _order_list = list(reversed([item[0] for item in _r_order_list]))
+    return _order_list
+
 
 if __name__ == "__main__":
-    print list_sort([1,1,1,1,2,2,2,2,3,4,4,5])
+    print list_sort([1,1,2,2,3])
+    print list_sort_b([1,1,2,2,3])
