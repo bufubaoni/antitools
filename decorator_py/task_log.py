d9592c7784c08c1697c986c6432f1528eb413686
alex <bufubaoni@163.com>
2017-03-10 09:41:52 +0800

    python decorator execution order

--- decorator_py/task_log.py
+++ decorator_py/task_log.py
@@ -3,10 +3,8 @@
 # Created by Alex on 2016/9/18
 
 def task_log(task):
-    print(task)
+    print(1)
     def decorator(*a, **k):
-        print(a)
-        print(k)
         return task(*a, **k)
     return decorator
 
@@ -14,18 +12,17 @@ def task_log_para(para):
     print(para)
     def wrapping(task):
         def decorator(*a,**k):
-            print(a)
-            print(k)
             return task(*a,**k)
         return decorator
     return wrapping
 
+
 @task_log
-@task_log_para("para")
+@task_log_para("para 1")
 def test(a):
     print(a)
     return a
 
 
 if __name__ == '__main__':
-    print(test("asdffff"))
+    test("asdffff")
