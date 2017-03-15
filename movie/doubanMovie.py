d286a4dd09e760c325f56c1cbab8f18df0fdd5a4
alex <bufubaoni@163.com>
2017-03-15 08:26:32 +0800

    douban

--- movie/doubanMovie.py
+++ movie/doubanMovie.py
@@ -13,18 +13,7 @@ pt = PrettyTable()
 pt.field_names=[u"编号",u"名称"]
 pt.align[u"名称"]="l"
 session = requests.Session()
-# url = "https://movie.douban.com/top250"
-# content = session.get(url).content
-# text = pq(content)("ol.grid_view>li>div.item")
-# code = pq(text[0])("div.pic>em").text()
-# title = pq(text[0])("div.info>div.hd>a").text()
-# code = u"1"
-# title = u"这个".encode("utf8")
-# pt.add_row((code,title))
-# print(pt)
-# with open("test.txt","a") as f:
-#     f.write(str(pt))
-# print content
+
 for i in range(0,10):
     url = "https://movie.douban.com/top250"
     if i == 0:
