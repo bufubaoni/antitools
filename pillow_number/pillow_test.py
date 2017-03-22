850bbbc31e5811529a117bca49e8cbfdd2fb7856
chenxin <bufubaoni@163.com>
2017-03-22 09:59:29 +0800

    simple img code gernarater

--- pillow_number/pillow_test.py
+++ pillow_number/pillow_test.py
@@ -2,14 +2,52 @@
 # -*- coding: utf-8 -*-
 # Created by alex on 2017/3/21
 from random import Random
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
-number = "455613"
-im = Image.new('RGBA', (100, 40), (255,) * 4)
+number = "4556"
 
-r = Random()
-draw = ImageDraw.ImageDraw(im)
 
-draw.text((0, 0), number, fill=(r.randint(0, 255), r.randint(0, 255), r.randint(0, 255)))
+# im = Image.new('RGBA', (100, 40), (255,) * 4)
+#
+# r = Random()
+# draw = ImageDraw.ImageDraw(im)
+#
+# draw.text((0, 0), number, fill=(r.randint(0, 255), r.randint(0, 255), r.randint(0, 255)))
+#
+# im.save("test.jpg")
 
-im.save("test.jpg")
+
+def code_img(code, size):
+    r = Random()
+    code = code
+    len_code = len(code)
+
+    font = ImageFont.truetype("Essence_Sans.ttf", size)
+
+    font_width, font_height = font.getsize(code)
+    font_width += size / 2
+    print font_width, font_height
+    img = Image.new("RGBA", (font_width, font_height), (255,) * 4)
+
+    draw = ImageDraw.ImageDraw(img)
+
+    draw.text((size/10, -size/10), code, font=font, fill=(0, 0, 0))
+
+    params = [1,
+              0,
+              0,
+              0,
+              1 - float(r.randint(1, 10)) / 100,
+              0,
+              0.001,
+              float(r.randint(1, 2)) / 500
+              ]
+    print params
+    img = img.transform((font_width, font_height), Image.PERSPECTIVE, params)
+    img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+
+    img.save("test.jpg")
+
+
+if __name__ == "__main__":
+    code_img(number, 35)
