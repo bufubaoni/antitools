a310d76d6b879f594c4cb7f0ecc89dc0abe6771b
chenxin <bufubaoni@163.com>
2017-03-31 15:18:15 +0800

    set and get attr some wrong

--- django_decorator/django_rest.py
+++ django_decorator/django_rest.py
@@ -25,6 +25,44 @@ def restful(para, result):
     return wrapping
 
 
+class RField(object):
+    def __init__(self, field_name, verbose_name=None, field_type="str", null=True, blank=True, validators=None):
+        self._field_name = field_name,
+        self._verbose_name = verbose_name if verbose_name else field_name
+        self._field_type = field_type
+        self._null = null
+        self._blank = blank
+        self._validators = validators
+
+        self._value = None
+        self._convert = {"str": str,
+                         "int": int}
+
+    @property
+    def value(self):
+        return self._convert[self._field_type](self._value)
+
+    @value.setter
+    def value(self, value):
+        print 1
+        if not self._validators:
+            self._value = value
+        else:
+            if self._validators(value):
+                self._value = value
+            else:
+                raise ValueError("%s value error", self._field_name)
+
+    # def __set__(self, instance, value):
+    #     print 1
+    #
+    #     self.value = value
+    #
+    # def __get__(self, instance, owner):
+    #     print 2
+    #     return self.value
+
+
 class JsonDict(dict):
     def __getattr__(self, attr):
         try:
@@ -49,5 +87,18 @@ def test(self, request, para, result, info):
     print para.username
 
 
+class Test(object):
+    def __init__(self):
+        self.username = RField("username", validators=lambda x: True if x < 20 else False)
+
+
 if __name__ == "__main__":
-    test("self", {"username": "username", "password": "password"})
+    # test("self", {"username": "username", "password": "password"}
+    t = Test()
+    t.username = 100
+    print t.username
+    # print type(b.a)
+    # field = RField("username", validators=lambda x: True if x < 20 else False)
+    # field = 10
+    # print field._field_name
+    # print type(field)
