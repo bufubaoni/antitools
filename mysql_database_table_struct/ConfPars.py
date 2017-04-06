f3585df07a90fa53e3804b6f75e7e434bd88410e
chenxin <bufubaoni@163.com>
2017-04-06 13:47:10 +0800

    update

--- mysql_database_table_struct/ConfPars.py
+++ mysql_database_table_struct/ConfPars.py
@@ -4,16 +4,13 @@
 import ConfigParser
 import json
 
-
 conf = ConfigParser.ConfigParser()
 conf.readfp(open("init.conf"))
 
 DB_URL = conf.get("mysql", "url")
 
-VERSON_FILE_NAME = conf.get("versonfile", "filename")
+OUTPUT_FILE = conf.get("excelfile", "filename")
 
-OUTPUT_EXCEL = conf.get("excelfile", "filename")
+TABLES = json.loads(conf.get("tables", "tables"))
 
-DATABASENAME = conf.get("db2excel","databasename")
-
-NO_NEED_TABLES = json.loads(conf.get("db2excel","noneedtables"))
+SCHEMA = conf.get("schema", "schema")
--- mysql_database_table_struct/export.py
+++ mysql_database_table_struct/export.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/4/6
+from mysql_model import sys_tab
+from ConfPars import TABLES, SCHEMA, OUTPUT_FILE
+import json
+
+
+def gettables(schema):
+    return {table: getcolumns(table, schema) for table in TABLES}
+
+
+def getcolumns(table, schema):
+    columns = sys_tab((sys_tab.COLUMNS.TABLE_NAME == table) & (sys_tab.COLUMNS.TABLE_SCHEMA == schema)). \
+        select(sys_tab.COLUMNS.COLUMN_NAME,
+               sys_tab.COLUMNS.IS_NULLABLE,
+               sys_tab.COLUMNS.DATA_TYPE,
+               sys_tab.COLUMNS.COLUMN_TYPE,
+               sys_tab.COLUMNS.TABLE_SCHEMA)
+    columns = {column.COLUMN_NAME: column.DATA_TYPE for column in columns if column.IS_NULLABLE=="NO"}
+    if "id" in columns:
+        columns.pop("id")
+
+    return columns
+
+
+def export(content):
+    with open(OUTPUT_FILE, "a") as f:
+        f.write(content)
+
+
+def main():
+    schema = SCHEMA
+    export(json.dumps(gettables(schema)))
+
+
+if __name__ == "__main__":
+    main()
