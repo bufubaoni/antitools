4acd7ea5e4e13d54583b71da3d7a6aa61809ff40
chenxin <bufubaoni@163.com>
2017-05-04 10:30:18 +0800

    invoke dynamic change paramaters

--- learnpython/foolq.py
+++ learnpython/foolq.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/4/26
-a = {"a": 1, "b": "c", "c": "q"}
-print "{a},{b}".format(**a)
-# KeyError
-print "{a}{b}".format(a="asdf")
+# a = {"a": 1, "b": "c", "c": "q"}
+# print "{a},{b}".format(**a)
+# # KeyError
+# print "{a}{b}".format(a="asdf")
+TEST = "test"
+
+
+def test():
+    print TEST
+
+
+if __name__ == "__main__":
+    test()
