230495e1824a6114ccd583d8f794cbb000e18be3
chenxin <bufubaoni@163.com>
2017-06-09 17:45:57 +0800

    update none type pass

--- learnpython/pytest.py
+++ learnpython/pytest.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 # Created by Alex on 2016/10/18
 def attr():
-    setattr(attr,"dict0",1)
-    print getattr(attr,"dict0")
+    setattr(attr, "dict0", 1)
+    print getattr(attr, "dict0")
     print locals()
 
 
-
-
-
 if __name__ == '__main__':
-    attr()
+    pd = attr()
+    print type(pd)
