0b5b21cc5591bfff4a4e5cb386ce2d3cb99f70f6
chenxin <bufubaoni@163.com>
2017-06-20 13:41:20 +0800

    defaultdict

---
+++ learnpython/defautldic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by ChenXin on 2017/6/20
+from collections import defaultdict
+
+dic =defaultdict(list)
+
+if __name__ =="__main__":
+    print dic["a"].append("12333")
+    print dic
