d9a2325aae4a1b9fbf9fdc01e129a7416943dfdc
chenxin <bufubaoni@163.com>
2017-06-28 16:30:33 +0800

    flask simple test

---
+++ service_test/service_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by ChenXin on 2017/6/28
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
