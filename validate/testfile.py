5d905b733a4ca2e57367ba93dff6ff819be68783
chenxin <bufubaoni@163.com>
2017-07-24 13:38:38 +0800

    update test file for validator

--- validate/testfile.py
+++ validate/testfile.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 from cerberus import Validator
 
-schema = {'name': {'type': "string", 'regex': "[0-9]+"}}
+schema = {'name': {'type': "string", 'regex': "[0-9]+"},
+          "password": {'type': "string", 'regex': "[0-9a-zA-Z]{6:20}", "empty": False, 'nullable': False,
+                       'required': True}}
 
 v = Validator(schema)
 if __name__ == '__main__':
-    print v.validate({"name": u"1"})
+    if not v.validate({"name": u"1"}):
+        print v.errors
+    else:
+        print "ok"
