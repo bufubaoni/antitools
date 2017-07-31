7c98186607e6d288a6994b855a52ce3e59e4d8a1
chenxin <bufubaoni@163.com>
2017-07-31 17:16:19 +0800

    use delete command

--- redis_test/redis_client.py
+++ redis_test/redis_client.py
@@ -10,5 +10,7 @@ r = redis.StrictRedis(host="192.168.85.130", port=6379, db=0)
 # r.append("key","dddddd")
 r.sadd("a","asdf")
 # print r["a"]
-print r.smembers("info_fire_authorities")
+# print r.smembers("info_fire_authorities")
 print r.sismember("a","b")
+r.delete("info_manometer")
+print r.smembers("info_manometer")
