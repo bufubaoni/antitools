3ca81d5ec54d75c410b45e969dae246e26388ac4
chenxin <bufubaoni@163.com>
2017-08-02 09:04:33 +0800

    update sta for

--- learnpython/sta_for.py
+++ learnpython/sta_for.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/7/20
+# for ... in ... else ...
+# the else code block exec when for loop exit normally
 list1 = [1, 2, 3]
 for i in list1:
     break
