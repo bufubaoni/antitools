fee21f93e055b888eaca9ebbfdf21ee893277811
chenxin <bufubaoni@163.com>
2017-08-02 09:02:39 +0800

    for loop sta

--- learnpython/sta_for.py
+++ learnpython/sta_for.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/7/20
-list1=[]
+list1 = [1, 2, 3]
 for i in list1:
+    break
     print i
 else:
-    print "else"
+    print i
