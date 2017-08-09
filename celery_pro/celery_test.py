9bf6e02371c08bae7294af0207c19a12ecb218ec
chenxin <bufubaoni@163.com>
2017-08-09 11:32:47 +0800

    update celery

---
+++ celery_pro/celery_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by ChenXin on 2017/8/8
+from celery import Celery
+
+app = Celery(broker= "amqp://alex:123456@192.168.85.130:5672/")
+
+
+# app.connection("192.168.85.130")
+
+@app.task
+def add(x, y):
+    return x + y
+
+
+# #
+if __name__ == '__main__':
+    app.worker_main()
