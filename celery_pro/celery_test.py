8d606ccb46c18ece3d0ccbc34dc87d554e428638
chenxin <bufubaoni@163.com>
2017-08-18 11:14:38 +0800

    celery start

--- celery_pro/celery_test.py
+++ celery_pro/celery_test.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/8/8
-from celery import Celery
-
-app = Celery(broker= "amqp://alex:123456@192.168.85.130:5672/")
-
-
+from celery.app import Celery
+app = Celery("test",broker="amqp://alex:123456@192.168.85.130:5672/")
 # app.connection("192.168.85.130")
 
 @app.task
-def add(x, y):
-    return x + y
-
-
-# #
+def add(x,y):
+    return x+y
+#
 if __name__ == '__main__':
     app.worker_main()
