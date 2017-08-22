28886ef314b4e5921b82e7670fd942661911f319
chenxin <bufubaoni@163.com>
2017-08-22 09:48:11 +0800

    celery pickle serial

--- celery_pro/client.py
+++ celery_pro/client.py
@@ -3,4 +3,9 @@
 # Created by ChenXin on 2017/8/18
 from celery_test import add
 
-print add.delay(12,15)
+# print add.delay(12,15)
+
+from base64 import b64decode
+import pickle
+
+print pickle.loads(b64decode("gAJ9cQEoVQZzdGF0dXNxAlUHU1VDQ0VTU3EDVQl0cmFjZWJhY2txBE5VBnJlc3VsdHEFSxlVB3Rhc2tfaWRxBlUkNmQ5YmVkMGEtNGNmZi00ZTJiLWE3MDgtNzMyNTUwYWI4MzYzcQdVCGNoaWxkcmVucQhddS4="))
