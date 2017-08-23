ec7a97d4db3a77b835b1f3de4e938dd90b92848c
chenxin <bufubaoni@163.com>
2017-08-23 15:41:03 +0800

    random food

--- learnpython/pika_learn.py
+++ learnpython/pika_learn.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/8/18
+import pika
+from pika.credentials import PlainCredentials,ExternalCredentials
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.85.130',credentials=PlainCredentials("alex","123456")),)
+channel = connection.channel()
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello Worldccccccc!')
--- random_food/random_food.py
+++ random_food/random_food.py
@@ -5,25 +5,23 @@ import random
 import time
 import sys
 
-
 _ver = sys.version_info
 
-is_py2 = (_ver[0]==2)
-is_py3 = (_ver[0]==3)
+is_py2 = (_ver[0] == 2)
+is_py3 = (_ver[0] == 3)
 
-#add your food in list
+# add your food in list
 foodlist = [u"1", u"2", u'3', u'4', u'5']
 
 
 def get_random_number(seed, len_foodlist):
     random.seed(seed)
-    random_number = random.randint(0, len_foodlist-1)
+    random_number = random.randint(0, len_foodlist - 1)
     return random_number
 
 
 def get_food(foodlist, random_number):
-		return foodlist[random_number]
-	
+    return foodlist[random_number]
 
 
 def choocie_food(foodlist):
@@ -32,13 +30,14 @@ def choocie_food(foodlist):
     random_number = get_random_number(seed, len_foodlist)
     print(get_food(foodlist, random_number))
 
+
 if __name__ == '__main__':
     choocie_food(foodlist)
     if is_py2:
         key = raw_input("press any key to exit!")
     else:
         key = input("press any key to exit!")
-		
-		
-		
+
+
+
 # test git
