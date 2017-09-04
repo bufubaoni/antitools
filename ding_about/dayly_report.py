a4d54d3199f4e5578885f34c469dce8661deab57
chenxin <bufubaoni@163.com>
2017-09-04 17:32:14 +0800

    ding talk add referer

--- ding_about/dayly_report.py
+++ ding_about/dayly_report.py
@@ -3,6 +3,7 @@
 # Created by ChenXin on 2017/9/1
 from requests import Session
 import json
+
 session = Session()
 url = "https://landray.dingtalkapps.com/alid/reportpc/client/create"
 
@@ -16,19 +17,28 @@ schema = {"atperson": [], "attachmentUri": [], "templateid": "1576b88d2bba77b4b8
 header = {
     "Host": "landray.dingtalkapps.com",
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0",
-    "Cookie": ""
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json",
+    # "random-numbers": "15045169547111624406",
+    "X-Requested-With": "XMLHttpRequest",
+    "Referer": "https://landray.dingtalkapps.com/alid/app/reportpc/createreport.html?corpid=dingaeeaf7839044d740&comeFromInside=1",
+    "Cookie": "",
+    "Connection": "keep-alive"
 }
 
 
-def pub_report(finished="", realtime="", needblame="", userid=""):
+def pub_report(finished="", realtime="", needblame="", userid="01512522161218664"):
     schema["detail"][0]["value"] = finished
     schema["detail"][1]["value"] = realtime
     schema["detail"][2]["value"] = needblame
     schema["userid"] = userid
 
-    session.headers=header
+    session.headers = header
 
-    print session.post(url,data=json.dumps(schema)).text
+    print session.post(url, data=json.dumps(schema)).text
+
 
 if __name__ == '__main__':
     pub_report(finished="测试")
