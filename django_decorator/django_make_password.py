9e41ed9e3739ca0d63fcff6c1e4165ce343ea721
chenxin <bufubaoni@163.com>
2017-09-05 17:58:38 +0800

    update django setting

--- django_decorator/django_make_password.py
+++ django_decorator/django_make_password.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/9/5
 import sys, os
+
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_decorator.settings")
 from django.contrib.auth.hashers import make_password
-
 
 if __name__ == '__main__':
     pass
