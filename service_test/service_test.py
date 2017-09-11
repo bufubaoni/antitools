3a14182af031797f258b70d560a81729552f97f0
chenxin <bufubaoni@163.com>
2017-09-11 16:11:23 +0800

    update test service

--- service_test/service_test.py
+++ service_test/service_test.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/6/28
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
