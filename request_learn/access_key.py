7c355718b4e718cc104dcc2e443550723e7c0366
chenxin <bufubaoni@163.com>
2017-09-28 18:00:48 +0800

    udate blog

--- request_learn/access_key.py
+++ request_learn/access_key.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by ChenXin on 2017/9/27
-import os
+from flask import Flask
 from requests import Session
 from datetime import datetime
-import json
+
 
 apply_time = datetime.now().strftime("%Y%m%d%H%M%S%f")[:-3]
 
@@ -22,15 +22,23 @@ load_body = "app_platform=1&apply_time=20170927172443521&" \
             "code=B041597F4FC3CF828314B73269B546EC0A2690B03AF21E101B19D6E0CAF88548".format(apply_time=apply_time)
 session.headers = {}
 
-acckey_url="http://acs.corelines.cn/phone/v1/home/visiting/password/add?room_node_id=20232&household_token=e056df6d1c90478892b3c934ebae711d&visiting_time=20170927000000000&household_id=9308&apply_time=20170927173736609&visiting_name=&code=19DE0F7F0F65F10099B0FCB506E652FCE466F89048CD6F502CE287DE55A9D86C&visiting_phone="
+acckey_url = "http://acs.corelines.cn/phone/v1/home/visiting/password/add?" \
+             "room_node_id=20232&household_token=e056df6d1c90478892b3c934ebae711d&visiting_time={visiting_time}" \
+             "&household_id=9308&apply_time=20170927173736609&visiting_name=&" \
+             "code=19DE0F7F0F65F10099B0FCB506E652FCE466F89048CD6F502CE287DE55A9D86C&visiting_phone=".format(visiting_time=apply_time)
+
+app = Flask(__name__)
 
 
-def opare_door(url, data):
+def opare_door(url):
     print url
-    return session.post(url, data=data).json()
+    return session.post(url).text
+
+
+@app.route('/get_key')
+def hello_world():
+    return opare_door(acckey_url)
 
 
 if __name__ == '__main__':
-    # print json.dumps(opare_door(load_url+"?"+load_body,None), ensure_ascii=False)
-    json.dumps(opare_door(acckey_url,None), ensure_ascii=False)
-    # print json.dumps(opare_door(opendoor_url,None), ensure_ascii=False)
+    app.run(host="0.0.0.0",port=5000)
