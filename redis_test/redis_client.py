b3c12008c2c500d9b4ef0ad84fd9ed6af9458ac7
chenxin <bufubaoni@163.com>
2017-09-30 15:56:58 +0800

    update blog for jdango cache

--- redis_test/redis_client.py
+++ redis_test/redis_client.py
@@ -3,7 +3,7 @@
 # Created by ChenXin on 2017/7/20
 import redis
 
-r = redis.StrictRedis(host="192.168.85.131", port=6379, db=0)
+r = redis.StrictRedis(host="127.0.0.1", port=6379, db=0)
 
 # r.set("key", [])
 # r.expire("name", 10)
