da53f2a423442a33a6c1d9a5c8074ba77955d0ba
chenxin <bufubaoni@163.com>
2017-11-20 11:06:08 +0800

    update django run gevent

---
+++ django_decorator/run_gevent.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by ChenXin on 2017/5/11
+from gevent import monkey
+
+monkey.patch_all()
+from gevent.wsgi import WSGIServer
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xxx.settings")
+
+application = get_wsgi_application()
+server = WSGIServer(("0.0.0.0", 9001), application)
+print "Starting server on http://127.0.0.1:9001"
+server.serve_forever()
